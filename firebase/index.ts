bafef5dc58ee6b897d0d59c978110f0bbad612aa
NicolasDev ccxc <nsabogal@ccxc.us>
2021-04-20 16:04:41 -0500

    Upload images to firebase

--- firebase/index.ts
+++ firebase/index.ts
@@ -1,15 +1,23 @@
 import firebase from 'firebase/app';
 import 'firebase/storage';
+import 'firebase/database'
 
 const firebaseConfig = {
     apiKey: "AIzaSyAO5FD5MNVpnD2FTiZ_p1QkD6zH_UtAd4k",
     authDomain: "test-ccxc.firebaseapp.com",
+    databaseURL: "https://test-ccxc-default-rtdb.firebaseio.com",
     projectId: "test-ccxc",
     storageBucket: "test-ccxc.appspot.com",
     messagingSenderId: "119419853062",
     appId: "1:119419853062:web:0059581455b79904c5a687"
 };
 
-firebase.initializeApp(firebaseConfig);
+if(!firebase.apps.length){
+    firebase.initializeApp(firebaseConfig);
+}else{
+    firebase.app();
+}
+
 const storage = firebase.storage();
-export {storage , firebase as default};
+const database = firebase.database();
+export {storage , database , firebase as default};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,50 +1,87 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import {useState} from "react";
-import {storage} from '../firebase/index';
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import {useEffect, useState} from "react";
+import {storage, database} from '../firebase';
+import { v4 as uuidv4 } from 'uuid';
 
+// Import React FilePond
+import { FilePond, registerPlugin } from 'react-filepond';
+import 'filepond/dist/filepond.min.css'
+import FilePondPluginImageExifOrientation from 'filepond-plugin-image-exif-orientation'
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview'
+import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css'
 
+// Register the plugins
+registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview)
 
 
 export default function Home() {
-  const [image, setImage] = useState(null);
-  const [listImages, setListImages] = useState<string[]>([]);
+  const [imageFilePond, setImageFilePond] = useState([]);
+  const [url,setUrl] = useState();
+  const [list, setList] = useState([]);
 
-  const handleChange = (e) =>{
-      if(e.target.files[0]){
-        setImage(e.target.files[0])
-      }
+
+  // useEffect(()=>{
+  //     database.ref('images/').get().then((snapshot)=>{
+  //         if(snapshot.exists()){
+  //             setList(snapshot.val())
+  //         }
+  //     })
+  // });
+
+
+  const writeDatabase = (url, name) => {
+      database.ref(`images/${uuidv4()}`).set({
+          url,
+          name,
+      });
+  }
+  const server = {
+      process: (fieldName, file, metadata, load, error, progress, abort) => {
+
+          const task = storage.ref('images/' + file.name).put(file, {
+              contentType: 'image/jpeg',
+          });
+          task.on(
+
+              "state_changed",
+              snapshot => {
+                  progress(true,snapshot.bytesTransferred, snapshot.totalBytes)
+              },
+              error => console.log(error),
+              ()=>{
+                  storage
+                      .ref('images')
+                      .child(file.name)
+                      .getDownloadURL()
+                      .then(res => {
+                          setUrl(res);
+                          writeDatabase(res, file.name)
+                      })
+              },
+          );
+      },
+      load: url
   };
 
-  const handleUpload = () =>{
-    const uploadTask = storage.ref(`images/${image.name}`).put(image);
-    uploadTask.on(
-        "state_changed",
-        snapshot => {},
-        error => console.log(error),
-        ()=>{
-          storage
-              .ref('images')
-              .child(image.name)
-              .getDownloadURL()
-              .then(res => {
-                  listImages.push(res)
-                  setListImages([...listImages]);
-              })
-        }
-    );
-  }
-
-
   return <div>
-    <input type='file' onChange={handleChange}/> <br/>
-    <button onClick={handleUpload}>Upload</button>
-      {
-          listImages.map((src,index)=>{
-              return <div key={index}>
-                  <img src={src} alt="test" width={250}/>
-              </div>
-          })
-      }
+      <FilePond
+          files={imageFilePond}
+          onupdatefiles={setImageFilePond}
+          allowMultiple={false}
+          maxFiles={1}
+          server={server}
+          name={`files${Math.random()}`}
+          labelIdle='Drag & Drop your files or <span class="filepond--label-action">Browse</span>'
+      />
+
+      {/*<div>*/}
+      {/*    {*/}
+      {/*        list.map((image,index)=>{*/}
+      {/*            return <div>asdsa</div>*/}
+      {/*        }*/}
+      {/*    )*/}
+      {/*    }*/}
+      {/*</div>*/}
   </div>
 }
